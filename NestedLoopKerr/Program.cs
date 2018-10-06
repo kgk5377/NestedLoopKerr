c51725fefa1f6ac2c6e238bf172d8441c382dff9
kgk5377 <43886659+kgk5377@users.noreply.github.com>
2018-10-06 08:25:37 -0700

    Add project files.

---
+++ NestedLoopKerr/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+	
+
+namespace NestedLoopKerr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            
+            
+
+            Console.ReadLine();
+            
+        }
+    }
+}
